40dbca0535489771f8985270fa0f622e7c8f49ea
Yuxing Fei <yuxingfei@berkeley.edu>
2024-01-16 19:22:07 -0800

    formatting

--- src/dara/xrdml2xy.py
+++ src/dara/xrdml2xy.py
@@ -15,15 +15,23 @@ def xrdml2xy(file: Path, target_folder: Path = None) -> Path:
     with file.open("r", encoding="utf-8") as f:
         xrd_dict = xmltodict.parse(f.read())
         min_angle = float(
-            xrd_dict["xrdMeasurements"]["xrdMeasurement"]["scan"]["dataPoints"]["positions"][0]["startPosition"]
+            xrd_dict["xrdMeasurements"]["xrdMeasurement"]["scan"]["dataPoints"][
+                "positions"
+            ][0]["startPosition"]
         )
         max_angle = float(
-            xrd_dict["xrdMeasurements"]["xrdMeasurement"]["scan"]["dataPoints"]["positions"][0]["endPosition"]
+            xrd_dict["xrdMeasurements"]["xrdMeasurement"]["scan"]["dataPoints"][
+                "positions"
+            ][0]["endPosition"]
         )
 
-        intensities = xrd_dict["xrdMeasurements"]["xrdMeasurement"]["scan"]["dataPoints"]["counts"]["#text"]
+        intensities = xrd_dict["xrdMeasurements"]["xrdMeasurement"]["scan"][
+            "dataPoints"
+        ]["counts"]["#text"]
         intensities = np.array([float(val) for val in intensities.split()])
         angles = np.linspace(min_angle, max_angle, len(intensities))
-        np.savetxt(target_path.as_posix(), np.column_stack((angles, intensities)), fmt="%f")
+        np.savetxt(
+            target_path.as_posix(), np.column_stack((angles, intensities)), fmt="%f"
+        )
 
         return target_path
