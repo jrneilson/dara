82323affa04a6ecc4c3eceb891938818580bb70c
Matt McDermott <mcdermott@berkeley.edu>
2024-03-19 23:02:10 -0700

    update to Cif.init --> add Cif.filename; add new default Cif.name
    Includes formula and space group info now in the name; hopefully this is not too sluggish

--- src/dara/cif.py
+++ src/dara/cif.py
@@ -12,16 +12,58 @@ from pymatgen.io.cif import CifFile, CifParser
 
 
 class CifBlock(MSONable, CifBlockPymatgen):
-    """Thin wrapper around CifBlock from pymatgen to enable serialization."""
+    """Thin wrapper around CifBlock to enable serialization by subclassing MSONable."""
 
 
 class Cif(MSONable, CifFile):
     """Thin wrapper around CifFile from pymatgen to enable serialization."""
 
-    def to_file(self, path: str | Path | None = None):
-        """Save to .cif file."""
+    def __init__(
+        self, data: dict, orig_string: str | None = None, comment: str | None = None, filename: str | None = None
+    ) -> None:
+        """
+        Args:
+            data: dict of CifBlock objects.
+            orig_string: The original cif string.
+            comment: Comment string.
+            filename: Filename of the CIF file. Optional; helps for tracking provenance.
+        """
+        super().__init__(data, orig_string, comment)
+        self.filename = filename or ""
+
+        try:
+            struct = CifParser.from_str(self.orig_string).parse_structures()[0]
+            formula = struct.composition.reduced_formula
+            sg = struct.get_space_group_info()[1]
+            self._name = f"{formula}_{sg}"
+        except Exception:
+            raise ValueError("CIF file structure can not be successfully parsed!")
+
+    @classmethod
+    def from_file(cls, path: str | Path) -> CifFile:
+        """
+        Read Cif from a path.
+
+        Args:
+            path: File path to read from.
+
+        Returns
+        -------
+            CifFile object
+        """
+        obj = super().from_file(path)
+        obj.filename = str(Path(path).stem)
+        return obj
+
+    def to_file(self, path: str | Path | None = None) -> None:
+        """Save to .cif file.
+
+        Args:
+            path: Path to save to. If None, will use the filename attribute (if available) or default to the name
+                attribute ([formula]_[spacegroup]).
+        """
         if path is None:
-            path = f"{self.name}.cif"
+            path = f"{self.filename}.cif" if self.filename else f"{self.name}.cif"
 
         with open(path, "w") as f:
             f.write(str(self))
@@ -31,16 +73,9 @@ class Cif(MSONable, CifFile):
         return Structure.from_str(str(self), fmt="cif", **kwargs)
 
     @property
-    def name(self):
+    def name(self) -> str:
         """Name of file (acquired either from top of file or from structure's formula)."""
-        name = next(iter(self.data.keys()))
-        if not name:
-            try:
-                name = CifParser.from_str(self.orig_string).parse_structures()[0].composition.reduced_formula
-            except Exception:
-                raise ValueError("CIF file is missing header and structure not successfully parsed!")
-
-        return name
+        return self._name
 
     @classmethod
     def from_str(cls, string) -> CifFile:
@@ -64,5 +99,5 @@ class Cif(MSONable, CifFile):
 
         return cls(dct, string)
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f"Cif[{self.name}]"
