6940d6fe9fd7b341d7feeaed896d9b71ef606200
Yuxing Fei <yuxingfei@berkeley.edu>
2024-04-06 12:13:24 -0700

    ADD benchmark script

--- scripts/parse_icsd.py
+++
@@ -1,8 +0,0 @@
-"""Parse the ICSD and return unique, oldest CIFs at room temperature."""
-from dara.parse_icsd import ICSDParser
-
-PATH_TO_ICSD = "/Users/mcdermott/Documents/ICSD"
-
-if __name__ == "__main__":
-    print("Parsing ICSD...")
-    ICSDParser(PATH_TO_ICSD).parse_all_cifs_with_icsd_codes_and_years()
