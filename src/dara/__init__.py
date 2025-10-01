7be3ee89e9695d8e333c68418a52d901afe5eaec
Yuxing Fei <yuxingfei@berkeley.edu>
2025-10-01 07:59:20 -0700

    Update version to 'dara-xrd' in __init__.py

--- src/dara/__init__.py
+++ src/dara/__init__.py
@@ -6,5 +6,5 @@ from dara.refine import RefinementPhase, do_refinement, do_refinement_no_saving
 from dara.search import search_phases
 from dara.settings import DaraSettings
 
-__version__ = version("dara")
+__version__ = version("dara-xrd")
 SETTINGS = DaraSettings()
